c6b618d6de211159617e0f0433962219e950c681
juan <juan@stripe.com>
2020-06-02 14:26:29 +0100

    Parchis MVP

--- javascripts/game.js
+++ javascripts/game.js
@@ -3,6 +3,40 @@ function randomNum() {
     num = Math.floor(Math.random() * 6) + 1;
     let dice = document.getElementById("dice");
     dice.style.backgroundImage = "url(assets/images/" + num + ".png)";
-    console.log(num);
 };
 
+// dragElement (document.getElementById("redpawn1"));//todo: how to specify the element you'd like to move.
+
+function dragElement (elmnt) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id)) {
+        document.getElementById(elmnt.id).onmousedown = dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
